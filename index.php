cbb042411248d345a41c1f64a22ad7a3fb10bded
Ryan McCahan <ryan@mccahan.net>
2015-06-03 17:07:35 -0600

    Plain page template

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()): ?>
-  <?php while (have_posts()): the_post(); ?>
-    <?php the_content(); ?>
-  <?php endwhile; ?>
+  <div class="container content-sm">
+    <?php while (have_posts()): the_post(); ?>
+      <div class="headline"><h2><?php the_title(); ?></h2></div>
+      <div class="row">
+        <div class="col-mid-12 md-margin-bottom-50">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    <?php endwhile; ?>
+  </div>
 <?php endif; ?>
 
 <?php get_footer();
