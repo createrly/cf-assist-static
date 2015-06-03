afc01cbc775c8631477b7a6ba9c015a4de0284f1
Ryan McCahan <ryan@mccahan.net>
2015-06-03 16:53:29 -0600

    Header/footer

---
+++ footer.php
@@ -0,0 +1,75 @@
+    <!--=== Footer Version 1 ===-->
+    <div class="footer-v1">
+      <div class="footer">
+          <div class="container">
+              <div class="row">
+                  <div class="copyright">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-6">
+                      <p>
+                          <?php echo date('Y'); ?> &copy; All Rights Reserved.
+                          <?php $menuLocations = get_nav_menu_locations(); ?>
+                          <?php if (isset($menuLocations['footer'])): ?>
+                            <?php $footerMenu = wp_get_nav_menu_object($menuLocations['footer']);
+                              $menuItems = wp_get_nav_menu_items($footerMenu->term_id);
+                              $menuLinks = array();
+                              foreach ($menuItems as $item)
+                                $menuLinks[] = "<a href='{$item->url}'>{$item->title}</a>";
+                            ?>
+                            <?php echo implode(' | ', $menuLinks); ?>
+                          <?php endif; ?>
+                      </p>
+                      <p>Website by <a href="http://www.createrly.com">Createrly</a></p>
+                  </div>
+
+                  <!-- Social Links -->
+                  <div class="col-md-6">
+                      <ul class="footer-socials list-inline">
+                          <li>
+                              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook">
+                                  <i class="fa fa-facebook"></i>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Linkedin">
+                                  <i class="fa fa-linkedin"></i>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter">
+                                  <i class="fa fa-twitter"></i>
+                              </a>
+                          </li>
+
+                      </ul>
+                  </div>
+                  <!-- End Social Links -->
+              </div>
+          </div>
+      </div>
+              </div>
+          </div>
+      </div><!--/footer-->
+
+      <!--/copyright-->
+    </div>
+  <!--=== End Footer Version 1 ===-->
+  </div><!--/wrapper-->
+
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/dist/CF-Assist.min.js"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function() {
+      	App.init();
+        OwlCarousel.initOwlCarousel();
+        ParallaxSlider.initParallaxSlider();
+    });
+  </script>
+  <!--[if lt IE 9]>
+    <script src="<?php echo get_template_directory_uri(); ?>/static/assets/plugins/respond.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/static/assets/plugins/html5shiv.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/static/assets/plugins/placeholder-IE-fixes.js"></script>
+  <![endif]-->
+  <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?> &raquo; <?php is_front_page() ? '' : wp_title(''); ?></title>
+  <link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600&amp;subset=cyrillic,latin'>
+  <link rel='stylesheet' type='text/css' href='<?php echo get_template_directory_uri(); ?>/dist/style.css'>
+
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+  <div class="wrapper">
+    <!--=== Header ===-->
+    <div class="header">
+        <div class="container">
+            <!-- Logo -->
+            <a class="logo" href="<?php echo get_home_url(); ?>" style="float:left;">
+                <img src="<?php echo get_template_directory_uri(); ?>/static/assets/img/cfassist/logo.jpg" alt="Logo" style="height:50px;">
+            </a>
+            <!-- End Logo -->
+
+            <!-- Toggle get grouped for better mobile display -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="fa fa-bars"></span>
+            </button>
+            <!-- End Toggle -->
+        </div><!--/end container-->
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse mega-menu navbar-responsive-collapse">
+            <div class="container">
+                <ul class="nav navbar-nav">
+                  <?php $menuLocations = get_nav_menu_locations(); ?>
+                  <?php if (isset($menuLocations['primary'])): ?>
+                    <?php $headerMenu = wp_get_nav_menu_object($menuLocations['primary']);
+                      $menuItems = wp_get_nav_menu_items($headerMenu->term_id);
+                      //var_dump($menuItems);
+                      $menuLinks = array();
+                      foreach ($menuItems as $item)
+                      {
+                        if ($item->menu_item_parent == 0)
+                          $menuLinks[$item->db_id] = array('title' => $item->title, 'url' => $item->url, 'children' => array(), 'page_id' => $item->object_id);
+                        else
+                          $menuLinks[$item->menu_item_parent]['children'][] = array('title' => $item->title, 'url' => $item->url, 'page_id' => $item->object_id);
+                      }
+
+                      foreach ($menuLinks as $link): ?>
+
+                      <li <?php if (count($link['children']) > 0): ?> class="dropdown"<?php endif; ?>>
+                        <?php if (count($link['children']) > 0): ?>
+                          <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown">
+                            <?php echo $link['title']; ?>
+                          </a>
+                          <ul class="dropdown-menu">
+                            <?php foreach ($link['children'] as $childLink): ?>
+                              <li><a href="<?php echo $childLink['url']; ?>"><?php echo htmlentities($childLink['title']); ?></a></li>
+                            <?php endforeach; ?>
+                          </ul>
+                        <?php else: ?>
+                          <a href="<?php echo $link['url']; ?>"><?php echo htmlentities($link['title']); ?></a>
+                        <?php endif; ?>
+                      </li>
+
+                    <?php endforeach; ?>
+                  <?php endif; ?>
+                </ul>
+            </div><!--/end container-->
+        </div><!--/navbar-collapse-->
+    </div>
+    <!--=== End Header ===-->
