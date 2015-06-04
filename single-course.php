69c8e3e374a352fb30506720797aad1cc72df2ef
Ryan McCahan <ryan@mccahan.net>
2015-06-04 00:12:30 -0600

    Course listings

--- single-course.php
+++ single-course.php
@@ -1,11 +1,16 @@
-<?php get_header(); ?>
+<?php
+/*
+ * Template Name: Course
+ */
+
+get_header(); ?>
 
 <?php if (have_posts()): ?>
   <div class="container content-sm">
     <?php while (have_posts()): the_post(); ?>
       <div class="headline"><h2><?php the_title(); ?></h2></div>
       <div class="row">
-        <div class="col-mid-12 md-margin-bottom-50">
+        <div class="col-md-12 md-margin-bottom-50">
           <?php the_content(); ?>
         </div>
       </div>
