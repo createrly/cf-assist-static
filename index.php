653f22785b015fe4fa68c8efa35ca9801678ab6d
Ryan McCahan <ryan@mccahan.net>
2015-06-09 23:22:16 -0600

    Refactoring slider

--- index.php
+++ index.php
@@ -3,30 +3,7 @@
 <?php if (is_front_page()): ?>
   <?php /* Slider */ ?>
   <?php if (have_rows('image_sliders')): ?>
-    <div class="slider-inner">
-      <div id="da-slider" class="da-slider">
-        <?php while (have_rows('image_sliders')): the_row(); ?>
-          <div class="da-slide" style="background:transparent url('<?php the_sub_field('image'); ?>');background-size:cover;">
-            <h2>
-              <?php $lines = explode("\n", get_sub_field('heading'));
-              for ($i = 0; $i < count($lines); $i++) if ($lines[$i] === '') unset($lines[$i]);
-              for ($i = 0; $i < count($lines); $i++): ?>
-                <i><?php echo trim(htmlentities($lines[$i])); ?></i><?php if ($i < count($lines)-1): ?><br><?php endif; ?>
-              <?php endfor; ?>
-            </h2>
-            <?php if (strlen(get_sub_field('subheading')) > 0): ?>
-              <p>
-                <?php $lines = explode("\n", get_sub_field('subheading'));
-                for ($i = 0; $i < count($lines); $i++) if ($lines[$i] === '') unset($lines[$i]);
-                for ($i = 0; $i < count($lines); $i++): ?>
-                  <i><?php echo trim(htmlentities($lines[$i])); ?></i><?php if ($i < count($lines)-1): ?><br><?php endif; ?>
-                <?php endfor; ?>
-              </p>
-            <?php endif; ?>
-          </div>
-        <?php endwhile; ?>
-      </div>
-    </div>
+    <?php get_template_part('slider'); ?>
   <?php endif; ?>
 
   <?php /* Call to Action */ ?>
