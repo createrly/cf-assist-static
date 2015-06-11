8c04a62a536afbc32b85abe67bc44b9ae90b80a7
Ryan McCahan <ryan@mccahan.net>
2015-06-11 00:39:54 -0600

    Single blog post template

---
+++ single.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<div class="container content blog-page blog-item">
+  <?php while (have_posts()): the_post(); ?>
+    <?php if (has_post_thumbnail()): ?>
+      <div class="blog-img">
+        <?php the_post_thumbnail(null, array('class' => 'img-responsive')); ?>
+      </div>
+    <?php endif; ?>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <div class="blog-tags">
+      <ul class="list-unstyled list-inline blog-info">
+        <li><i class="fa fa-calendar"></i> <?php the_date(); ?></li>
+        <li><i class="fa fa-pencil"></i> <?php the_author(); ?></li>
+        <li><i class="fa fa-tags"></i> <?php the_tags(); ?></li>
+      </ul>
+    </div>
+    <?php the_content(); ?>
+  <?php endwhile; ?>
+</div>
+
+<?php get_footer(); ?>
