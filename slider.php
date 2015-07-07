11c45d6695424adca1d187dfe2df07e8cd2ccd1f
Ryan McCahan <ryan@mccahan.net>
2015-07-07 14:13:39 -0600

    Make homepage slider linkable

--- slider.php
+++ slider.php
@@ -2,13 +2,13 @@
   <div id="da-slider" class="da-slider">
     <?php while (have_rows('image_sliders')): the_row(); ?>
       <div class="da-slide" style="background:transparent url('<?php the_sub_field('image'); ?>');background-size:cover;">
-        <h2>
+        <a href="<?php the_sub_field('link'); ?>"><h2>
           <?php $lines = explode("\n", get_sub_field('heading'));
           for ($i = 0; $i < count($lines); $i++) if ($lines[$i] === '') unset($lines[$i]);
           for ($i = 0; $i < count($lines); $i++): ?>
             <i><?php echo trim(htmlentities($lines[$i])); ?></i><?php if ($i < count($lines)-1): ?><br><?php endif; ?>
           <?php endfor; ?>
-        </h2>
+        </h2></a>
         <?php if (strlen(get_sub_field('subheading')) > 0): ?>
           <p>
             <?php $lines = explode("\n", get_sub_field('subheading'));
